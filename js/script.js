26eb3d6852c9b8cfeb9f0d5cf4b03a33b2690860
leoxuVancouver <99696932+leoxuVancouver@users.noreply.github.com>
2022-05-12 13:09:09 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+/******************************************
+*****************************************/
+
+/*** 
+ * `quotes` array 
+***/
+
+q1={quote:"Stay hungry, stay foolish.",source:"Steve Jobs",citation:"washingtonpost",year:"2011"};
+q2={quote:"Well done is better than well said.",source:"Benjamin Franklin",year:"1717"};
+q3={quote:"Whenever you find yourself on the side of the majority, it is time to pause and reflect.",source:"Mark Twain",citation:" BrainyQuote.com"};
+q4={quote:"Don’t count the days, make the days count.",source:"Muhammad Ali"};
+q5={quote:"The future belongs to those who prepare for it today.",source:"Malcolm X"};
+const quotes=[q1,q2,q3,q4,q5];
+
+console.log(quotes);
+
+/***
+ * `getRandomQuote` function
+***/
+function getRandomQuote(){
+    let randomNumber=Math.floor(Math.random()*quotes.length);
+    return quotes[randomNumber];
+}
+
+
+/***
+ * `printQuote` function
+***/
+function printQuote(){
+    let randomQuote=getRandomQuote();
+    let PtoHTML1="<p class='quote'>"+randomQuote.quote+"</p>"+"<p class='source'>"+
+    randomQuote.source;
+    //
+    if(randomQuote.citation!==undefined){
+        PtoHTML1+="<span class='citation'>"+randomQuote.citation+
+        "</span>";
+    }
+    if(randomQuote.hasOwnProperty('year')){
+        PtoHTML1+="<span class='year'>"+randomQuote.year+
+        "</span>";
+    }
+    PtoHTML1+="</p>";
+    document.getElementById('quote-box').innerHTML=PtoHTML1;
+}
+
+
+/***
+ * click event listener for the print quote button
+ * The code will look like the following. You need to complete it.
+
+***/
+
+document.getElementById('load-quote').addEventListener("click",printQuote);
